f5c1a742c1c16bcb64cabe28e9af3540c4ddde78
Oleg Vikharev <olegvihr444@gmail.com>
2023-02-21 11:07:50 +0300

    commented out the minus function

--- uheba_01/for_lek04/first/nums.py
+++ uheba_01/for_lek04/first/nums.py
@@ -1,8 +1,8 @@
 def plus(a, b):
     return a + b
 
-def minus(a, b):
-    return a - b
+# def minus(a, b):
+#     return a - b
 
 def multiply(a, b):
     return a * b
@@ -15,7 +15,7 @@ def modulo(a, b):
 
 if __name__ == '__main__':
     print(plus(1, 2))
-    print(minus(1, 2))
+    # print(minus(1, 2))
     print(multiply(1, 2))
     print(divide(1, 2))
     print(modulo(1, 2))
