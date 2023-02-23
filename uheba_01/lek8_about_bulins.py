7682d3353bbfbc036098a90a7a87f0440f6c8298
Oleg Vikharev <olegvihr444@gmail.com>
2023-02-23 17:00:57 +0300

    A little about bulins, True and False

--- uheba_01/lek8_about_bulins.py
+++ uheba_01/lek8_about_bulins.py
@@ -5,9 +5,13 @@ print(answers[x > 0])
 falsy = [False, 0, 0.0, '', None, [], {}, set(), tuple(), range(0)]
 for e in falsy:
     print(bool(e))
+
+
 class Cat:
     def meow(self):
         print('meow')
+
+
 cat = Cat()
 print(bool(cat))
 x, y = 1, 1
@@ -18,9 +22,12 @@ if word in ('YES', 'Yes', 'yes'):
     print('YES')
 else:
     print('NO')
-def check(x)->bool:
+
+
+def check(x) -> bool:
     print(f'{x}>0 is {bool}')
     return x > 0
+
 
 if check(1) and check(1) and check(2) and check(3) and check(4) and check(5) and check(6):
     print('all good')
@@ -28,6 +35,9 @@ if check(1) and check(1) and check(2) and check(3) and check(4) and check(5) and
 if check(-1) or check(-1) or check(-1) or check(3) or check(4) or check(5) or check(6):
     print('all good')
 
-def is_even(x)->bool:
+
+def is_even(x) -> bool:
     return x % 2 == 0
+
+
 print(is_even(2))
