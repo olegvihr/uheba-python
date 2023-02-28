1cf5ee39e4fac16b6ddf6abac7f61159bf410620
olegv <olegvihr444@gmail.com>
2023-02-28 11:28:19 +0300

    lesson added: default arguments in functions

--- uheba_01/lek12_arguments_in_functions.py
+++ uheba_01/lek12_arguments_in_functions.py
@@ -1,8 +1,30 @@
-def print_n_times(value: str, n: int = 10):
+from time import time, sleep
+
+
+def print_n_times(value: str, n: int = 4):
     for _ in range(n):
         print(value)
+
+
+def some_function():
+    print("some_function called")
+    return 1
+
+
+# def calc(time_=[]):  # не правильно, т.к. [] имеет изменяемый тип
+def calc(time_=None):  # правильно
+    if time_ is None:
+        time_= []
+    time_.append(1)
+    return time_
 
 
 if __name__ == "__main__":
     print_n_times("Hello")
     print_n_times("World", n=2)
+    print(1, end=',')
+    print(1)
+    print(calc())
+    print(calc())
+    sleep(2)
+    print(calc())
