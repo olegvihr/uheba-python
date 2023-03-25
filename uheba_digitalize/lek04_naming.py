4e1a505f230d8b422e9f55e60ab8f71288807d1a
Oleg Vikharev <olegvihr444@gmail.com>
2023-03-25 14:08:17 +0300

    Digitalize, added lesson: Naming variables, classes and methods in code

--- uheba_digitalize/lek04_naming.py
+++ uheba_digitalize/lek04_naming.py
@@ -4,16 +4,17 @@ from telebot.types import User
 customers = User.objects.filter()  # клиенты
 paying_customers = User.objects.filter()  # клиенты, которые платят
 
+
 def get_active_users():
     pass
+
 
 def get_paying_companies():
     pass
 
+
 company.is_paying
 user.is_active
-
-
 
 if city.aoding == '0c5b2444-.....':  # 1 вариант
     pass
